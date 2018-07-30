5b3dd8d5722b382a5204bd7edca2e1b250753c59
Gustavo Dutra Santana <gustavo.santana@ringa.com.br>
2018-07-30 13:48:38 -0300

    Commit to pull

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
+  post 'user_token' => 'user_token#create'
 	resources :reports
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
